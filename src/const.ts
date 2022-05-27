4f403febe5af8465e071260a53c1a6643de53e3e
coldTOUFU <coldtoufu.dev@gmail.com>
2022-05-27 13:12:29 +0900

    add: 時間割生成で使う曜日関連の定数追加。

---
+++ src/const.ts
@@ -0,0 +1,14 @@
+export namespace Const {
+  export const MON = 0;
+  export const TUE = 1;
+  export const WED = 2;
+  export const THU = 3;
+  export const FRI = 4;
+  export const SAT = 5;
+  export const SUN = 6;
+
+  export const DAY_OF_WEEK_CHARS = ["月", "火", "水", "木", "金", "土", "日"] as const;
+
+  const dayOfWeeks = [MON, TUE, WED, THU, FRI, SAT, SUN] as const;
+  export type DayOfWeek = typeof dayOfWeeks[number];
+}
